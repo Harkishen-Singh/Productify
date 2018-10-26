db73b7a603d4675b16b709eb85c6dcae0ec2896d
C-Anirudh <canirudh16@gmail.com>
2018-10-27 00:17:24 +0530

    deleted old file.

--- extension/js/options.js
+++
@@ -1,8 +0,0 @@
-$(  function($scope){
-    $('#lang-btn').click(function(){
-        var x = $('#lang-btn').val();
-        $('#lang-btn').val('');
-        if (x)
-            $scope.lang = x;
-    });
-});
