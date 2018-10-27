b8b7d065234afff62c4b9eb0816bc9e2a4a2f6f9
C-Anirudh <canirudh16@gmail.com>
2018-10-27 14:22:21 +0530

    Corrected dictionary fuctionality andmodified popup

---
+++ extension/js/front_page.js
@@ -0,0 +1,11 @@
+function name(){
+    chrome.storage.local.get('mainMemory', (details) => {
+        let Urls = details.mainMemory.blockedWebsites;
+        let len = Urls.length;
+        let words = details.mainMemory.wordId;
+        document.getElementById('blocked-num').innerHTML = len - 1;
+        document.getElementById('dictionary-num').innerHTML = words;
+    });
+}
+    
+setInterval(name,500);
