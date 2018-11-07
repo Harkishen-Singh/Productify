f7927944bb891554717d1fa855b8fc3c8b33748e
Harkishen Singh <hsingh@localhost.localdomain>
2018-11-07 23:35:20 +0530

    added null warnings

--- extension/tsc__/_ts_files/options.ts
+++ extension/tsc__/_ts_files/options.ts
@@ -1,38 +1,48 @@
 chrome.storage.local.get('mainMemory', (details) => {
-    let allUrls = details.mainMemory.allUrls, 
-        allURlsNode = '',allBlockedNodes = '',
-        blockedWebsites = details.mainMemory.blockedWebsites;
+    let allUrls:any = details.mainMemory.allUrls, 
+        allURlsNode: string = '',allBlockedNodes:string = '',
+        blockedWebsites:any = details.mainMemory.blockedWebsites;
     console.warn(allUrls)
-    for(let i =1; i< allUrls.length; i++) {
-        allURlsNode += '<div class="row" style="border-bottom: 1px solid black;margin:5px;padding-bottom: 10px;overflow-x:auto;max-width:90%;">' +
-            '<div class="col-md-10" style="font-size:15px;"><span style="padding-right:5px;background-color:red;color:white;border-radius:5px;padding:5px;" >'
-            +parseInt(allUrls[i].time)/60+' mins</span> <b> '+ allUrls[i].url + ' </b></div>' + 
-            '<div class="col-md-2"><button id="'+allUrls[i].url +'" class="btn btn-danger">Block</button></div></div>';
+
+    if (!(allUrls.length===1 && allUrls[0].url==="https://www.defaultsomethingss.com/*")) {
+        for(let i =1; i< allUrls.length; i++) {
+            allURlsNode += '<div class="row" style="border-bottom: 1px solid black;margin:5px;padding-bottom: 10px;overflow-x:auto;max-width:90%;">' +
+                '<div class="col-md-10" style="font-size:15px;"><span style="padding-right:5px;background-color:red;color:white;border-radius:5px;padding:5px;" >'
+                +parseInt(allUrls[i].time)/60+' mins</span> <b> '+ allUrls[i].url + ' </b></div>' + 
+                '<div class="col-md-2"><button id="'+allUrls[i].url +'" class="btn btn-danger">Block</button></div></div>';
+            
+        }
         
+        document.getElementById('all_urls_view').innerHTML=allURlsNode;
+        for(let i =1; i< allUrls.length; i++) {
+            document.getElementById(allUrls[i].url).addEventListener('click',addBlocking, false);
+        }
+    } else {
+        document.getElementById('all_urls_view').innerHTML= 'No websites Visited Yet.';
     }
     
-    document.getElementById('all_urls_view').innerHTML=allURlsNode;
-    for(let i =1; i< allUrls.length; i++) {
-        document.getElementById(allUrls[i].url).addEventListener('click',addBlocking, false)
-    }
 
     // blocked ones
-
     console.warn(blockedWebsites)
-    for(let i =0; i< blockedWebsites.length; i++) {
-        if (blockedWebsites[i] === 'https://www.defaultsomethingss.com/*') 
-            continue
-        allBlockedNodes += '<div class="row" style="border-bottom: 1px solid black;margin:5px;padding-bottom: 10px;">' +
-            '<div class="col-md-10" style="font-size:15px;"> <b> '+ blockedWebsites[i] + ' </b></div>' + 
-            '<div class="col-md-2"><button id="'+blockedWebsites[i] +'" class="btn btn-success">Allow</button></div></div>';
-        
+    
+    if (!(blockedWebsites.length===1 && blockedWebsites[0]==='https://www.defaultsomethingss.com/*')) {
+        console.warn(blockedWebsites)
+        for(let i =0; i< blockedWebsites.length; i++) {
+            if (blockedWebsites[i] === 'https://www.defaultsomethingss.com/*') 
+                continue
+            allBlockedNodes += '<div class="row" style="border-bottom: 1px solid black;margin:5px;padding-bottom: 10px;">' +
+                '<div class="col-md-10" style="font-size:15px;"> <b> '+ blockedWebsites[i] + ' </b></div>' + 
+                '<div class="col-md-2"><button id="'+blockedWebsites[i] +'" class="btn btn-success">Allow</button></div></div>';
+            
+        }
+        document.getElementById('blocked_urls_view').innerHTML=allBlockedNodes;
+        for(let i =0; i< blockedWebsites.length; i++) {
+            if(!(blockedWebsites[i] === 'https://www.defaultsomethingss.com/*'))
+            document.getElementById(blockedWebsites[i]).addEventListener('click',removeBlocking, false)
+        }
+    } else {
+        document.getElementById('blocked_urls_view').innerHTML= 'No websites Blocked.';
     }
-    document.getElementById('blocked_urls_view').innerHTML=allBlockedNodes;
-    for(let i =0; i< blockedWebsites.length; i++) {
-        if(!(blockedWebsites[i] === 'https://www.defaultsomethingss.com/*'))
-        document.getElementById(blockedWebsites[i]).addEventListener('click',removeBlocking, false)
-    }
-
     let words = details.mainMemory.dictionaryWords;
     let wordsNode = '';
     for(let i =0; i< words.length; i++) {
@@ -92,9 +102,9 @@ function removeBlocking(this: HTMLElement, element: any) {
 
 function articleViewHandler() {
     chrome.storage.local.get('savedArticlesCodeZero', (details) => {
-        let allSavedArticles = details.savedArticlesCodeZero.savedArticles;
-        let orderedList = document.createElement('ol');
-        let totalArticles = allSavedArticles.length;
+        let allSavedArticles: any = details.savedArticlesCodeZero.savedArticles;
+        let orderedList: HTMLElement = document.createElement('ol');
+        let totalArticles: number = allSavedArticles.length;
         // for(let i=0; i< totalArticles; i++) {
         //     let x = document.createElement('p');
         //     x.id = allSavedArticles[i].URL;
@@ -102,14 +112,23 @@ function articleViewHandler() {
         //     x.addEventListener('click', assignActionsArticles, false )
         //     document.getElementById('articleTitle').appendChild(x)
         // }
-        for(let i=0; i< totalArticles; i++) {
-            let List = document.createElement('li');
-            List.id = allSavedArticles[i].URL;
-            List.innerHTML = allSavedArticles[i].URL + '<br>';
-            List.addEventListener('click', assignActionsArticles, false )
-            orderedList.appendChild(List);
+        if (totalArticles) {
+            for(let i=0; i< totalArticles; i++) {
+                let List = document.createElement('li');
+                List.id = allSavedArticles[i].URL;
+                List.innerHTML = allSavedArticles[i].URL + '<br>';
+                List.addEventListener('click', assignActionsArticles, false )
+                orderedList.appendChild(List);
+            }
+            document.getElementById('articleTitle').appendChild(orderedList);
+        } else {
+            let element: HTMLElement = document.createElement('p');
+            element.innerHTML = 'No articles saved Yet.';
+            document.getElementById('articleTitle').appendChild(element);
+            document.getElementById('articleBody').innerHTML = "<span style='margin-left:20px;'>Empty!<br> Please click the download Icon on the bottom left of the page"+
+                " to save the Article for offline view.</span>";
         }
-        document.getElementById('articleTitle').appendChild(orderedList);
+        
     })
 } articleViewHandler();
 
